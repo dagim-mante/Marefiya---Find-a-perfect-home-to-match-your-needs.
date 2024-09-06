df02a1fd286728494ce198170a288f3d4ff92fe9
dagim-mante <dagimawimantefardo@gmail.com>
2024-09-06 09:10:10 +0300

    added stylings to emails

--- server/actions/email.ts
+++ server/actions/email.ts
@@ -21,8 +21,34 @@ export const sendVerificationEmail = async(email:string, token:string) => {
     const mailOptions = {
       from: process.env.EMAIL_ACCOUNT,
       to: email,
-      subject: "2FA Token",
-      html: `<h3>Verify your account</h3><p>To verify your account click this <a href=${confirmLink}>link.</a></p>`,
+      subject: "Confirm Email",
+      html: `
+        <style>
+            body{
+                padding: 0;
+                margin: 0;
+                box-sizing: border-box;
+            }
+            @media (max-width: 500px) {
+                a#cta{
+                    width: 90%;
+                    align-self: center;
+                }
+            }
+        </style>
+        <div id="container" style="display:flex;flex-direction: column;margin: 0;padding:20px;">
+            <img src="logo.svg" alt="Marefiya logo" style="width: 100px;height: 50px;"/>
+            <h1 style="margin: 0; padding-left: 20px;margin-top: 20px;font-size: 28px;font-weight: bold;">Welcome to Marefiya</h1>
+            <p style="margin: 0;padding-left: 20px;margin-top: 10px;font-size: 16px;font-weight: lighter;">We are happy that you have joined our community, we have created your account you just have to confirm your email by clicking the button below.</p>
+            <a href="${confirmLink}" 
+                style="padding-left: 20px; text-decoration: none;padding: 15px 20px;background-color: blue;color: white;margin-top: 20px; display: inline-block;width: 40%;align-self: center;text-align: center;border-radius: 10px;"
+                target="_blank"
+                id="cta"
+            >
+                Confirm Email
+            </a>
+        </div>
+      `,
     }
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
@@ -39,8 +65,34 @@ export const sendPasswordResetEmail = async(email:string, token:string) => {
   const mailOptions = {
     from: process.env.EMAIL_ACCOUNT,
     to: email,
-    subject: "2FA Token",
-    html: `<h3>Change your password</h3><p>To reset password click this <a href=${confirmLink}>link.</a></p>`,
+    subject: "Forgot Password",
+    html: `
+      <style>
+        body{
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+        @media (max-width: 500px) {
+            a#cta{
+                width: 90%;
+                align-self: center;
+            }
+        }
+      </style>
+      <div id="container" style="display:flex;flex-direction: column;margin: 0;padding:20px;">
+        <img src="logo.svg" alt="Marefiya logo" style="width: 100px;height: 50px;"/>
+        <h1 style="margin: 0; padding-left: 20px;margin-top: 20px;font-size: 28px;font-weight: bold;">Reset your password</h1>
+        <p style="margin: 0;padding-left: 20px;margin-top: 10px;font-size: 16px;font-weight: lighter;">If you requested to reset your password reset your password click the button below if you didn't ignore this Email.</p>
+        <a href="${confirmLink}" 
+            style="padding-left: 20px; text-decoration: none;padding: 15px 20px;background-color: blue;color: white;margin-top: 20px; display: inline-block;width: 40%;align-self: center;text-align: center;border-radius: 10px;"
+            target="_blank"
+            id="cta"
+        >
+            Reset Password
+        </a>
+      </div>
+    `,
   }
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
@@ -55,8 +107,24 @@ export const sendTwoFactorEmail = async(email:string, token:string) => {
   const mailOptions = {
     from: process.env.EMAIL_ACCOUNT,
     to: email,
-    subject: "2FA Token",
-    html: `<h3>Your 6 digit token is ${token}</h3>`,
+    subject: "Two Factor Authentication",
+    html: `
+      <style>
+        body{
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+      </style>
+      <div id="container" style="display:flex;flex-direction: column;margin: 0;padding:20px;">
+          <img src="logo.svg" alt="Marefiya logo" style="width: 100px;height: 50px;"/>
+          <h1 style="margin: 0; padding-left: 20px;margin-top: 20px;font-size: 28px;font-weight: bold;">2FA Token</h1>
+          <p style="margin: 0;padding-left: 20px;margin-top: 10px;font-size: 16px;font-weight: lighter;">You have tried to login into your account. Since your account is two factor authentication protected here is your 6 digit code.</p>
+          <p style="margin: 0;padding-left: 20px;margin-top: 20px;font-weight: 400;letter-spacing: 5px;font-size: 22px;align-self: center;color: blue;">
+              ${token}
+          </a>
+      </div>
+    `,
   }
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
