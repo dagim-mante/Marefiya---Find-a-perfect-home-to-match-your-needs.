0326740da6667853d2b63f65c3050add7ff8f00d
dagim-mante <dagimawimantefardo@gmail.com>
2024-09-08 14:30:55 +0300

    hero section

--- components/assets/assets.tsx
+++ components/assets/assets.tsx
@@ -82,77 +82,80 @@ export default function Assets({
         return favourites.find(f => f.userId === userId)?.id
     }
     return (
-        <main className="max-w-screen-xl mx-auto p-4 sm:p-4 md:p-8">
-            {assets ? (
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
-                    {assets.map(asset => (
-                        <div key={asset.id} className="rounded overflow-hidden shadow-lg flex flex-col">
-                            <div className="relative h-64">
-                                    <Image width="96" height="95" className="w-full h-full"
-                                        src={asset.assetImages[0].url}
-                                        alt={asset.title} />
-                                    <div
-                                        className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
+        <>
+            <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-300 mt-8">Discover</h2>
+            <main className="max-w-screen-xl mx-auto p-4 sm:p-4 md:p-8">
+                {assets ? (
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+                        {assets.map(asset => (
+                            <div key={asset.id} className="rounded overflow-hidden shadow-lg flex flex-col">
+                                <div className="relative h-64">
+                                        <Image width="96" height="95" className="w-full h-full"
+                                            src={asset.assetImages[0].url}
+                                            alt={asset.title} />
+                                        <div
+                                            className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
+                                        </div>
+                                    {session ? (
+                                        <>
+                                            {isInmyFavourite(asset.favourites, session.user.id) ? (
+                                                <span
+                                                    onClick={() => {
+                                                        removeFromFavourites(
+                                                            isInmyFavourite(asset.favourites, session.user.id)!,
+                                                            asset.id,
+                                                            session.user.id
+                                                        )
+                                                    }}
+                                                >
+                                                    <Heart className="text-xs absolute top-0 right-0 bg-transparent p-0 cursor-pointer text-transparent mt-3 mr-3 fill-red-500 transition duration-300 ease-in-out" />
+                                                </span>
+                                            ) : (
+                                                <span
+                                                    onClick={() => {
+                                                        addToFavourite(asset.id,  session.user.id)
+                                                    }}
+                                                >
+                                                    <Heart className="text-xs absolute top-0 right-0 bg-transparent p-0 cursor-pointer text-white mt-3 mr-3 hover:fill-red-500 hover:text-transparent transition duration-300 ease-in-out" />
+                                                </span>
+                                            )}
+                                        </>
+                                    ): null}
+                                </div>
+                                <div className="px-6 py-3">
+                                    <Link 
+                                        href={`/assets/${asset.id}`}
+                                        className="font-medium text-lg inline-block hover:text-primary transition duration-200 ease-in-out mb-2"
+                                    >
+                                        {asset.title}
+                                    </Link>
+                                    <div className="flex items-center justify-between">
+                                        <Badge>
+                                            {`For ${asset.type?.slice(0, 1).toUpperCase()}${asset.type?.slice(1)}`}
+                                        </Badge>
+                                        <p className="font-bold text-xs">
+                                            {formatPrice(asset.price, asset.type, asset.rentType)}
+                                        </p>
                                     </div>
-                                {session ? (
-                                    <>
-                                        {isInmyFavourite(asset.favourites, session.user.id) ? (
-                                            <span
-                                                onClick={() => {
-                                                    removeFromFavourites(
-                                                        isInmyFavourite(asset.favourites, session.user.id)!,
-                                                        asset.id,
-                                                        session.user.id
-                                                    )
-                                                }}
-                                            >
-                                                <Heart className="text-xs absolute top-0 right-0 bg-transparent p-0 cursor-pointer text-transparent mt-3 mr-3 fill-red-500 transition duration-300 ease-in-out" />
-                                            </span>
-                                        ) : (
-                                            <span
-                                                onClick={() => {
-                                                    addToFavourite(asset.id,  session.user.id)
-                                                }}
-                                            >
-                                                <Heart className="text-xs absolute top-0 right-0 bg-transparent p-0 cursor-pointer text-white mt-3 mr-3 hover:fill-red-500 hover:text-transparent transition duration-300 ease-in-out" />
-                                            </span>
-                                        )}
-                                    </>
-                                ): null}
-                            </div>
-                            <div className="px-6 py-3">
-                                <Link 
-                                    href={`/assets/${asset.id}`}
-                                    className="font-medium text-lg inline-block hover:text-primary transition duration-200 ease-in-out mb-2"
-                                >
-                                    {asset.title}
-                                </Link>
-                                <div className="flex items-center justify-between">
-                                    <Badge>
-                                        {`For ${asset.type?.slice(0, 1).toUpperCase()}${asset.type?.slice(1)}`}
-                                    </Badge>
-                                    <p className="font-bold text-xs">
-                                        {formatPrice(asset.price, asset.type, asset.rentType)}
-                                    </p>
+                                </div>
+                                <div className="px-6 py-3 flex flex-row items-center justify-between gap-1">
+                                    <span className="py-1 text-xs font-regular dark:text-muted-foreground text-gray-900 mr-1 flex flex-row items-center">
+                                        <MapPin />
+                                        <span className="ml-1">Akaki Kality, Addis Ababa</span>
+                                    </span>
+
+                                    <span className="py-1 text-xs font-regular dark:text-muted-foreground text-gray-900 mr-1 flex flex-row items-center">
+                                        <MessageCircle />
+                                        <span className="ml-1">{asset.reviews.length} Reviews</span>
+                                    </span>
                                 </div>
                             </div>
-                            <div className="px-6 py-3 flex flex-row items-center justify-between gap-1">
-                                <span className="py-1 text-xs font-regular dark:text-muted-foreground text-gray-900 mr-1 flex flex-row items-center">
-                                    <MapPin />
-                                    <span className="ml-1">Akaki Kality, Addis Ababa</span>
-                                </span>
-
-                                <span className="py-1 text-xs font-regular dark:text-muted-foreground text-gray-900 mr-1 flex flex-row items-center">
-                                    <MessageCircle />
-                                    <span className="ml-1">{asset.reviews.length} Reviews</span>
-                                </span>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            ) : (
-                <h2>No Assets.</h2>
-            )}
-        </main>
+                        ))}
+                    </div>
+                ) : (
+                    <h2>No Assets.</h2>
+                )}
+            </main>
+        </>
     )
 }
--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -110,41 +110,61 @@ export default function HeroSection(){
     );
 
     return (
-        <div className="w-full">
-            <Autocomplete
-                initialState={{
-                    query: (params.get('query') as string) || '',
-                }}
-                openOnFocus={true}
-                placeholder="Search for assets..."
-                detachedMediaQuery="(max-width: 24px)"
-                classNames={{
-                  form: 'relative rounded-md shadow-sm flex-1',
-                  inputWrapperPrefix:
-                    'absolute inset-y-0 left-0 flex items-center pl-3',
-                  inputWrapperSuffix:
-                    'absolute inset-y-0 right-0 flex items-center pr-2',
-                  submitButton: 'h-5 w-5 text-gray-400',
-                  clearButton: 'h-5 w-5 text-gray-400',
-                  input:
-                    'block w-full rounded-md border-primary py-3 pl-10 sm:text-sm',
-                  panel:
-                    'dark:bg-gray-600 dark:text-white dark:hover:text-gray-500 absolute mt-2 lg:py-1 z-10 ring-1 ring-black ring-opacity-5 text-sm text-gray-500 bg-white lg:shadow-lg rounded-md overflow-y-scroll lg:max-h-96 md:text-md lg:text-md text-xs',
-                }}
-                navigator={{
-                    navigate({itemUrl}){
-                        router.push(itemUrl)
-                    }
-                }}
-                onSubmit={({state}) => {
-                    router.push(`/search?query=${state.query}`)
-                }}
-                plugins={[
-                    getQuerySuggestionsPlugin(),
-                    getRecentSearchesPlugin()
-                ]}
+      <div className="bg-gray-200 dark:bg-gray-800 rounded-md">
+        <div className="dark:bg-transparent">
+          <div className="mx-auto flex flex-col items-center py-12 sm:py-24">
+              <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col mb-3 sm:mb-10">
+                  <h1
+                      className="text-4xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-6xl text-center text-gray-800 dark:text-white font-black leading-10">
+                      Welcome to 
+                      <span className="text-violet-800 dark:text-violet-500"> Marefiya</span>.
+                  </h1>
+                  <p className="sm:mt-5 lg:w-10/12 text-gray-600 dark:text-gray-300 font-normal text-center text-xl">
+                      Lets find you a home. 
+                  </p>
+              </div>
+              <div className="flex w-11/12 md:w-8/12 xl:w-6/12">
+                  <div className="flex rounded-md w-full">
+                    <div className="w-full">
+                        <Autocomplete
+                            initialState={{
+                                query: (params.get('query') as string) || '',
+                            }}
+                            openOnFocus={true}
+                            placeholder="Search for assets..."
+                            detachedMediaQuery="(max-width: 24px)"
+                            classNames={{
+                              form: 'relative rounded-md shadow-sm flex-1',
+                              inputWrapperPrefix:
+                                'absolute inset-y-0 left-0 flex items-center pl-3',
+                              inputWrapperSuffix:
+                                'absolute inset-y-0 right-0 flex items-center pr-2',
+                              submitButton: 'h-5 w-5 text-gray-400',
+                              clearButton: 'h-5 w-5 text-gray-400',
+                              input:
+                                'block w-full rounded-md border-primary py-3 pl-10 sm:text-sm',
+                              panel:
+                                'dark:bg-gray-600 dark:text-white dark:hover:text-gray-500 absolute mt-2 lg:py-1 z-10 ring-1 ring-black ring-opacity-5 text-sm text-gray-500 bg-white lg:shadow-lg rounded-md overflow-y-scroll lg:max-h-96 md:text-md lg:text-md text-xs',
+                            }}
+                            navigator={{
+                                navigate({itemUrl}){
+                                    router.push(itemUrl)
+                                }
+                            }}
+                            onSubmit={({state}) => {
+                                router.push(`/search?query=${state.query}`)
+                            }}
+                            plugins={[
+                                getQuerySuggestionsPlugin(),
+                                getRecentSearchesPlugin()
+                            ]}
 
-            />
+                        />
+                    </div>
+                  </div>
+              </div>
+          </div>
         </div>
+      </div>
     )
 }
