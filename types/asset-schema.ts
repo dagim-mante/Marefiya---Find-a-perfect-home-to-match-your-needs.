b281ad58a0a451986f45f371a8ee94694732ef9c
dagim-mante <dagimawimantefardo@gmail.com>
2024-09-10 02:31:09 +0300

    deploy-test-2

--- types/asset-schema.ts
+++ types/asset-schema.ts
@@ -4,7 +4,7 @@ export const AssetSchema = z.object({
     id: z.number().optional(),
     title: z.string().min(4, {message: 'Title must be atleast 4 charchters.'}),
     description: z.string().min(40, {message: 'Description must be atleast 40 chrachters.'}),
-    type: z.enum(["rent", "sell"]).nullable(),
+    type: z.string().nullable(),
     price: z.coerce.number({message: 'Price must be a number.'})
             .positive({message: 'Price must be postive.'}),
     rentType: z.enum(["night", "week", "month"]).nullable(),
