2820310a192aa2a968a6055e3adbf62ee45d7151
dagim-mante <dagimawimantefardo@gmail.com>
2024-09-28 22:59:37 +0300

    analytics page age loading state

--- app/dashboard/analytics/[id]/loading.tsx
+++ app/dashboard/analytics/[id]/loading.tsx
@@ -5,7 +5,7 @@ export default function Dashboard(){
     return (
         <div className="h-[calc(100vh-9rem)]">
             <div className="px-4 py-6 flex lg:flex-row flex-col gap-4">
-                <Card className="h-96 flex-1 shrink-0">
+                <Card className="px-4 py-6 h-96 flex-1 shrink-0">
                     <Skeleton className="w-60 h-10 mb-1"/>
                     <Skeleton className="w-72 h-5 mb-1"/>
                     <div className="flex items-center gap-2 mb-3">
@@ -16,7 +16,7 @@ export default function Dashboard(){
                         <Skeleton className="w-full h-72"/>
                     </div>
                 </Card>
-                <Card className="h-96 flex-1 shrink-0">
+                <Card className="px-4 py-6 h-96 flex-1 shrink-0">
                     <Skeleton className="w-60 h-10 mb-1"/>
                     <Skeleton className="w-72 h-5 mb-1"/>
                     <div className="flex items-center gap-2 mb-3">
